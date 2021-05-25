5a0230bf4bfb1202908f84b386dfa62d974284e1
Kristinevalm <kristinevalm@gmail.com>
2021-05-25 11:15:04 +0300

    vd57

--- EmployeeManagment.Web/Pages/EditEmployeeBase.cs
+++ EmployeeManagment.Web/Pages/EditEmployeeBase.cs
@@ -3,15 +3,19 @@ using EmployeeManagment.Models;
 using EmployeeManagment.Web.Models;
 using EmployeeManagment.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace EmployeeManagment.Web.Pages
 {
     public class EditEmployeeBase : ComponentBase
     {
+        [CascadingParameter]
+        private Task<AuthenticationState> authenticationStateTask { get; set; }
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
         public string PageHeader { get; set; }
@@ -31,8 +35,17 @@ namespace EmployeeManagment.Web.Pages
         public NavigationManager NavigationManager { get; set; }
 
         protected async override Task OnInitializedAsync()
+        { 
+
+            var authenticationState = await authenticationStateTask;
+
+        if (!authenticationState.User.Identity.IsAuthenticated)
         {
-            int.TryParse(Id, out int employeeId);
+            string returnUrl = WebUtility.UrlEncode($"/editEmployee/{Id}");
+        NavigationManager.NavigateTo($"/identity/account/login?returnUrl={returnUrl}");
+        }
+
+                    int.TryParse(Id, out int employeeId);
 
             if (employeeId != 0)
             {
